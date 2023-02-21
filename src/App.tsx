4fd18bb64bab53d2420a12225c69e706eed325d2
Probmkr <69635586+Probmkr@users.noreply.github.com>
2023-02-21 11:25:09 +0000

    とりあえずコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import "./App.scss";
+import { PlayIcon, PauseIcon } from "./lib/svgs";
 
 const App: React.FC<Record<string, never>> = () => {
   return (
     <div className="App">
       <div className="timer">
         <div className="time">
-          <span id="min" className="time-num min">5</span>
-          <span className="sep">:</span>
-          <span id="sec" className="time-num sec">05</span>
-          <span className="sep">:</span>
-          <span id="msec" className="time-num msec">05</span>
+          <div className="time-wrap">
+            <span id="min" className="time-num min">
+              5
+            </span>
+            <span className="sep">:</span>
+            <span id="sec" className="time-num sec">
+              05
+            </span>
+            <span className="sep">:</span>
+            <span id="msec" className="time-num msec">
+              05
+            </span>
+          </div>
         </div>
         <div className="buttons">
-          <div className="start-pause"></div>
+          <div className="start-pause">
+            <PlayIcon />
+            <PauseIcon />
+          </div>
           <div className="next"></div>
         </div>
       </div>
