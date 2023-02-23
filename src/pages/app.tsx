b065a30bfdb2b68060913c3d936a67cf9b7347a3
Probmkr <Thanatos9911@gmail.com>
2023-02-23 17:16:34 +0900

    nodejs に移行

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.scss";
+import styles from "../styles/App.module.scss";
 import { PlayIcon, PauseIcon } from "../lib/svgs";
 import { Part, TypeJapanese } from "../types/part.d";
 
@@ -27,60 +27,60 @@ const App: React.FC<Record<string, never>> = () => {
     },
   ];
   return (
-    <div className="App">
-      <div className="timer">
-        <div className="time">
+    <div className={styles.App}>
+      <div className={styles.timer}>
+        <div className={styles.time}>
           <div
-            className="time-wrap"
+            className={styles.timeWrap}
             onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
               const range = document.createRange();
               range.selectNode(event.currentTarget);
               window.getSelection()?.addRange(range);
             }}
           >
-            <span id="min" className="time-num min">
+            <span id="min" className={`${styles.timeNum} ${styles.min}`}>
               5
             </span>
-            <span className="sep">:</span>
-            <span id="sec" className="time-num sec">
+            <span className={styles.sep}>:</span>
+            <span id="sec" className={`${styles.timeNum} ${styles.sec}`}>
               05
             </span>
-            <span className="sep">:</span>
-            <span id="msec" className="time-num msec">
+            <span className={styles.sep}>:</span>
+            <span id="msec" className={`${styles.timeNum} ${styles.msec}`}>
               05
             </span>
           </div>
         </div>
-        <div className="buttons">
+        <div className={styles.buttons}>
           <div
             className="start-pause"
             onClick={() => {
               const play = document.querySelector(
-                ".start-pause .bi-play-circle"
+                `.start-pause .bi-play-circle`
               );
               const pause = document.querySelector(
-                ".start-pause .bi-pause-circle"
+                `.start-pause .bi-pause-circle`
               );
-              play?.classList.toggle("hide");
-              pause?.classList.toggle("hide");
+              play?.classList.toggle(styles.hide);
+              pause?.classList.toggle(styles.hide);
             }}
           >
             <PlayIcon />
-            <PauseIcon className="hide" />
+            <PauseIcon className={styles.hide} />
           </div>
-          <div className="next"></div>
+          <div className={styles.next}></div>
         </div>
       </div>
-      <div className="sidebar">
-        <div className="flow">
+      <div className={styles.sidebar}>
+        <div className={styles.flow}>
           <ul>
             {flow.map((part: Part, idx: number) => {
               const date = new Date(part.duration * 1000 * 60);
               return (
-                <li key={idx} className={part.side}>
-                  <div className="wrapper">
-                    <div className="title">{TypeJapanese[part.type]}</div>
-                    <div className="duration">{`${date.getMinutes()}分${date.getSeconds()}秒`}</div>
+                <li key={idx} className={styles[part.side]}>
+                  <div className={styles.wrapper}>
+                    <div className={styles.title}>{TypeJapanese[part.type]}</div>
+                    <div className={styles.duration}>{`${date.getMinutes()}分${date.getSeconds()}秒`}</div>
                   </div>
                 </li>
               );
